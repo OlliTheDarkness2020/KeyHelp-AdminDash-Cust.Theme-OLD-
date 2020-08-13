bf8d170a977c79e9dc3a4cfe790113a34cd34067
OlliTheDarkness2020 <67396171+OlliTheDarkness2020@users.noreply.github.com>
2020-08-13 19:51:30 +0200

    DEV aus Master entfernt.

--- KeyHelp (Version 20.2-Dev)/assets/js/page_backup_repo.js
+++
@@ -1,9 +0,0 @@
-// Show / hide additional fields for selected repository type.
-$('select[name="type"]').on('change', function() {
-    var $this = $(this);
-
-    // Hide all.
-    $('[class^="app-show-with-type-"]').hide(0);
-    // Show selected.
-    $('.app-show-with-type-' + $this.val()).show(200);
-}).trigger('change');
